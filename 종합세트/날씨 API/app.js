df3576f9c8abaa6d042fbfbd2f1bb7250d02e350
BbbGoat <oomi9421@gmail.com>
2022-12-30 09:32:23 +0900

    ss

---
+++ 종합세트/날씨 API/app.js
@@ -0,0 +1,5 @@
+const toggleButton = document.getElementById('toggleButton');
+
+toggleButton.addEventListener('click', () => {
+  // Code to toggle the button's appearance and behavior goes here
+});
