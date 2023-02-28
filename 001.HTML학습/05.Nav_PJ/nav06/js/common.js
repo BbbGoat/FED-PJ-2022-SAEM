8bdedffa9c594a9f1fcfb24e8472497438143e66
BbbGoat <oomi9421@gmail.com>
2023-02-28 15:52:07 +0900

    Update common.js

--- 001.HTML학습/05.Nav_PJ/nav06/js/common.js
+++ 001.HTML학습/05.Nav_PJ/nav06/js/common.js
@@ -16,15 +16,16 @@ function loadFn() {
         GNB메뉴 객체 데이터를 이용한
         HTML태그 만들어 넣기!
     *********************************/
-    // 대상: .gnb
+    // 1. 대상선정: .gnb
     const gnb = document.querySelector(".gnb");
     // console.log(gnb);
 
-    // html코드 담을 변수
+    // 2. 변수 셋팅: html코드 담을 변수
     let hcode = "";
 
+    // 3. 코드의 구조화 생성하기
     hcode +=`<ul>`;
-    // 1. 상위메뉴 반복코드 생성
+    // 3-1. 상위메뉴 반복코드 생성
     // mdata객체를 가져와서 반복시킴 -> for in문!
     // console.log(mdata);
 
@@ -36,23 +37,30 @@ function loadFn() {
                 <a href="#">${tm}</a>
                 <div class="smenu">
                     <h2>
-                        ${tm}
+                        <div class="stit">${tm}<div>
+                        <a href="#">전체보기 ＞</a>
                     </h2>
                     <div class="swrap">
                         `;
-        // 2. 하위메뉴 반복코드                
+
+        // 3-2. 하위메뉴 반복코드                
         // -> 객체 데이터 이므로 for in문 사용!
         // -> mdata[tm] -> mdata[속성명] -> 속성값!
         // -> 속성값은 서브메뉴이고 객체로 구성됨!
         for (let sm in mdata[tm]){
             hcode +=
             `<dl>
-                <dt>${sm}</dt> `;
+                <dt>
+                    <a href="#">${sm}</a>
+                </dt> `;
 
-                // 3. 서브메뉴(최하위메뉴) 반복코드
+                // 3-3. 서브메뉴(최하위메뉴) 반복코드
                 // -> 서브메뉴는 배열이므로 for of 사용!
                 for (let sub of mdata[tm][sm]) {
-                    hcode += `<dd>${sub}</dd>`;
+                    hcode += 
+                    `<dd>
+                        <a href="#">${sub}</a>
+                    </dd>`;
                 } ///////////// for of문 ////////////
 
             hcode += 
@@ -67,10 +75,10 @@ function loadFn() {
             `;
 
     } //////////// for in문 ////////////////
-
+    
     hcode += "</ul>";
 
-    // GNB 박스에 출력하기
+    // 4. GNB 박스에 출력하기
     gnb.innerHTML = hcode;
     
 
