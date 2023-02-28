1c233e13327583225356f003b90f81fbefeecdf3
BbbGoat <oomi9421@gmail.com>
2023-02-28 15:35:31 +0900

    ss

--- 001.HTML학습/05.Nav_PJ/nav06/js/common.js
+++ 001.HTML학습/05.Nav_PJ/nav06/js/common.js
@@ -10,6 +10,68 @@ window.addEventListener("DOMContentLoaded", loadFn);
 
 function loadFn() {
 
+    console.log("로딩완료!");
 
+    /********************************* 
+        GNB메뉴 객체 데이터를 이용한
+        HTML태그 만들어 넣기!
+    *********************************/
+    // 대상: .gnb
+    const gnb = document.querySelector(".gnb");
+    // console.log(gnb);
+
+    // html코드 담을 변수
+    let hcode = "";
+
+    hcode +=`<ul>`;
+    // 1. 상위메뉴 반복코드 생성
+    // mdata객체를 가져와서 반복시킴 -> for in문!
+    // console.log(mdata);
+
+    for (let tm in mdata) { // tm은 mdata의 속성명
+
+        hcode+=
+            `
+            <li>
+                <a href="#">${tm}</a>
+                <div class="smenu">
+                    <h2>
+                        ${tm}
+                    </h2>
+                    <div class="swrap">
+                        `;
+        // 2. 하위메뉴 반복코드                
+        // -> 객체 데이터 이므로 for in문 사용!
+        // -> mdata[tm] -> mdata[속성명] -> 속성값!
+        // -> 속성값은 서브메뉴이고 객체로 구성됨!
+        for (let sm in mdata[tm]){
+            hcode +=
+            `<dl>
+                <dt>${sm}</dt> `;
+
+                // 3. 서브메뉴(최하위메뉴) 반복코드
+                // -> 서브메뉴는 배열이므로 for of 사용!
+                for (let sub of mdata[tm][sm]) {
+                    hcode += `<dd>${sub}</dd>`;
+                } ///////////// for of문 ////////////
+
+            hcode += 
+            `</dl>`;
+        } /////////// for in문 ///////////
+
+        hcode +=
+                        `
+                    </div>
+                </div>
+            </li>
+            `;
+
+    } //////////// for in문 ////////////////
+
+    hcode += "</ul>";
+
+    // GNB 박스에 출력하기
+    gnb.innerHTML = hcode;
+    
 
 } //////////////// loadFn 함수 ///////////////
