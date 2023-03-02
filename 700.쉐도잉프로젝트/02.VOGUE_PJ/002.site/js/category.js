432f6d1cf0b44f0a5542dc2cbd7f984f5b9b63ca
BbbGoat <oomi9421@gmail.com>
2023-03-02 15:14:18 +0900

    Update category.js

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/category.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/category.js
@@ -23,6 +23,7 @@ function loadFn() {
     const stit = document.querySelector(".stit");
     // (2) 서브메뉴
     const lnb = document.querySelector(".lnb");
+    // const lnb = document.querySelectorAll(".lnb li a");
     // (3) 내용 타이틀
     const contit = document.querySelectorAll(".icont h2");
     // (4) 컨텐츠 상위박스(카테고리 클래스 넣기)
@@ -30,7 +31,7 @@ function loadFn() {
     // (5) title요소(타이틀 내용에 카테고리명 앞에 추가)
     const titag = document.querySelector("title");
 
-    // console.log(stit,lnb,contit,cont,titag);
+    console.log(stit,lnb,contit,cont,titag);
 
     // 2. 메뉴데이타 (sinfo변수) 객체에서 카테고리값 선택하기
     const mdata = sinfo[pm];
@@ -43,6 +44,39 @@ function loadFn() {
     stit.innerText = mdata["제목"];
 
     // (2) LNB 메뉴 넣기
+    // 대상: .lnb -> lnb변수
+    // 코드넣기 :
+    // <ul><li><a href="#">메뉴</a></li></ul>
+    // mdata["메뉴"] 값이 "없음"이면 lnb를 지우고
+    // 아니면 배열값으로 메뉴가 있으므로 배열만큼
+    // 위의 ul>li>a 코드를 넣어 준다!
+
+    // 메뉴값 담기
+    let mvalue = mdata["메뉴"];
+    
+    if (mvalue === "없음") {
+        // LNB박스 제거!
+        lnb.remove();
+    } //////////// if ////////////////
+    else{ // 메뉴가 배열에 있음!
+        
+        let temp = "<ul>"; // 임시변수
+
+        // 메뉴 배열만큼 돌아서 코드생성!
+        mvalue.forEach((val)=>{ // val은 배열값!
+            temp += `
+                <li>
+                    <a href="#">${val}</a>
+                </li>
+            `;
+        }); /////// forEach ///////
+
+        temp += "</ul>";
+
+        // LNB박스에 html넣기!
+        lnb.innerHTML = temp;
+
+    } ///////////// else //////////////
 
     // (3) 내용 타이틀 넣기 : 대상 - contit변수 
     // -> h2 개수만큼 순번대로 mdata["타이틀"][순번]
