80e460fd45b411a736d5075e76589a6953bbbbbc
BbbGoat <oomi9421@gmail.com>
2023-03-14 10:21:32 +0900

    ss

--- 001.HTML학습/10.Ossomae_PJ/js/main.js
+++ 001.HTML학습/10.Ossomae_PJ/js/main.js
@@ -4,7 +4,33 @@
 window.addEventListener("DOMContentLoaded",()=>{
 
     console.log("로딩완료!");
+    const gbx = document.querySelector(".gbx");
 
+    // 오른쪽버튼
+    document.querySelector(".rb").onclick = () => {
+        console.log("오른쪽!");
+
+        // 이동대상 : .gbx > div
+        let tg = gbx.querySelectorAll("div");
+
+        // 첫번째 자식요소 div 맨뒤로이동
+        // appendChild(첫번째요소)
+        gbx.appendChild(tg[0]);
+
+    }; ///////// click ///////////////////
+
+    // 왼쪽버튼
+    document.querySelector(".lb").onclick = () => {
+        console.log("왼쪽!");
+
+        // 이동대상 : .gbx > div
+        let tg = gbx.querySelectorAll("div");
+
+        // 마지막 자식요소 div 맨앞이동
+        // insertBefore(마지막요소,첫번째요소)
+        gbx.insertBefore(tg[tg.length-1],tg[0]);
+
+    }; ///////// click ///////////////////
     
 }); ///////////// 로딩구역 //////////////////////
 /////////////////////////////////////////////////
