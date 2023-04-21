295173ff80b73deecff96b98c536430b64509ce5
BbbGoat <oomi9421@gmail.com>
2023-04-21 11:08:40 +0900

    Update main.js

--- 004.JS학습/Parallax_PJ/js/main.js
+++ 004.JS학습/Parallax_PJ/js/main.js
@@ -13,7 +13,7 @@ function loadFn() {
     // 2. 공통선택자함수
     const qs = (x) => document.querySelector(x);
     const qsa = (x) => document.querySelectorAll(x);
-    
+
     // 3. 등장위치리턴함수
     const retVal = (x) => {
         return x.getBoundingClientRect().top;
@@ -35,12 +35,64 @@ function loadFn() {
     // (2). 아이콘
     const tg2 = qsa(".icon");
 
-    // 7. 스크롤 이벤트함수 만들기
-    window.addEventListener("scroll", ()=>{
+    // 7. 패럴렉스 이동함수 //
+    const moveEl = (elpos,ele,setH) => {
 
-        // 테스트 - 3번째 텍스트 위치값
-        console.log(retVal(tg1[0]))    
+        // 전달값: elpos - 요소위치값 / ele - 요소 / setH - 지정한범위
+
+        
+        // 패럴렉스 범위 : 윈도우높이값 ~ 0까지!
+        // 화면에서 완전히 사라질때 범위는 0이 아니고
+        // 요소의 -(마이너스)높이값만큼 이다!
+        if (elpos < winH && elpos > 0) {
+
+            // 1. 위치이동값 계산
+            let x = setH - (elpos * setH) / winH;
+            console.log("x:",-x);
+
+            // 2. 해당요소에 위치이동 CSS 반영
+            ele.style.cssText = `
+                transform:translateY(${-x}px);
+            `;
+            // cssText 속성은 style 속성값을 직접 넣어준다!(css문법 그대로 사용)
+            
+            // 대상요소의 트랜스폼 Y축 이동
+            // 위치이동 계산원리
+            // -> 윈도우화면 : 위치값 = 지정한범위 : 실제이동값
+            // -> 윈도우가 1000px : 약 500px쯤의 위치 = 상자범위 200px : x?
+            // 실제이동값 = 위치값 * 지정한범위 / 윈도우화면
+            // -> x = elpos * setH2 / winH
+            // 이동수치는 0부터 서서히 증가해야 하므로 지정한범위수에서 빼준다!
+            // -> x = setH2 - (elpos * setH2) / winH
+
+        } ///// if : 패럴렉스 범위 ///////
+
+    }; //////////// moveEl ///////////////
+
+
+    // 8. 스크롤 이벤트함수 만들기
+    window.addEventListener("scroll", () => {
+
+        
+        // 대상1 : 글자박스 패럴렉스호출!
+        tg1.forEach((ele)=>{
+            moveEl(retVal(ele),ele,setH2)
+            // moveEl(위치값,요소,지정한범위)
+        });
+        // 대상2 : 글자박스 패럴렉스호출!
+        tg2.forEach((ele)=>{
+            moveEl(retVal(ele),ele,setH1)
+            // moveEl(위치값,요소,지정한범위)
+        });
         
     }); //////////// scroll 이벤트 ////////////////
+
+    // 로딩시 맨위로 이동하기
+    setTimeout(() => {
+        // 맨위로 이동
+        window.scrollTo(0,0);
+        // 부드러운 스크롤 위치값 반영!
+        pos = 0; // 안하면 새로고침시 튄다!
+    },400);
     
 } ////////////////////// loadFn 함수 ///////////////////
