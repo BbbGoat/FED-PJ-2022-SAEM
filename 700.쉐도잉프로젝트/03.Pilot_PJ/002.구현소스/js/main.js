367e827cdfa994f799dc3431226e003f12e57966
BbbGoat <oomi9421@gmail.com>
2023-05-01 13:13:37 +0900

    ss

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
@@ -53,9 +53,30 @@ new Vue({
         // 배너기능 호출
         banFn();
     }
-})
+}); ////////////// 상단영역 Vue 인스턴스 /////////////////////
 
 
+// ########### 하단영역 메뉴 뷰템플릿 셋팅하기 ########## //
+// 자식
+Vue.component("foot-comp",{
+    template: comData.barea
+}); ////////////////// 하단영역 Vue component /////////////////
 
+// ######### 하단영역 뷰 인스턴스 생성하기 ########## //
+// 부모
+new Vue({
+    el: "#info",
+    data:{},
+    // created 실행구역 : DOM 연걸 전!!
+    created: function(){
+        // DOM 연결 전 데이터 가공 작업
+        console.log("created구역");
+    }, 
+    // mounted 실행구역: DOM 연결 후!!
+    mounted: function(){
+        // 제이쿼리코드함수 호출
+        console.log("mounted구역");
+    }
+}); ////////////// 하단영역 Vue 인스턴스 /////////////////////
 
 
