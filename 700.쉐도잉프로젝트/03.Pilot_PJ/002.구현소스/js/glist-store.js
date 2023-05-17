3ebc5d9743369d84d3fed45a053087ede1b026d7
BbbGoat <oomi9421@gmail.com>
2023-05-17 13:20:55 +0900

    ss

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-store.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-store.js
@@ -8,11 +8,56 @@ const store = new Vuex.Store({
         // 서브데이터 셋업
         // 전체상품정보 전역화
         gdata: gdata,
-        
+        // 필터데이터용 배열변수
+        chkarr: [true,true,true],
+        // 필터데이터용 배열입력값 변수
+        selnm: ["","",""],
+        // 페이징용 변수
+        pnum: 0,
+        // 모어용 변수
+        mnum: 0,
+        // 모어버튼 표시변수
+        mbtn:true,
+
     },
     // state 데이터 변경 메서드구역!
     mutations: {
+        // 체크박스 체크시 처리메서드
+        resCheck(dt) {
+            console.log("배열데이터:",dt.chkarr);
+            // 3개의 체크박스 상태배열변수값에 따라
+            // 실제 조건에 들어갈 cat명을 넣어준다!
 
+            // v는 배열값인 true/false값이 들어옴!
+            dt.chkarr.forEach((v,i)=>{
+                if(v) {
+                    dt.selnm[i] = i == 0? "men" : i == 1? "women" : "style";
+                    // 조건1? 값1 : ((조건2? 값2) : 최종 else);
+                    // 중첩 삼항연산자 사용!! 자주쓰인다! 기억하기!
+                }
+                else {
+                    // 체크박스 체크안됨
+                    // 무조건 빈값을 할당!
+                    dt.selnm[i] = "";                    
+                }
+            });
+        }, ///////////////// resCheck ////////////////////
+
+        // 페이징 변수 업데이트 메서드
+        updatePaging(dt,pm) { // pm - 업데이트할 숫자
+            // pnum은 리스트 범위수
+            dt.pnum = pm;
+        }, //////////////// updatePaging /////////////////
+
+        // 모어 변수 업데이트 메서드
+        updateMore(dt,pm) { // pm - 업데이트할 숫자
+            // mnum은 리스트 범위수
+            dt.mnum = pm;
+
+            // 업데이트 후, 모어버튼 없애기
+            // $("#more").hide();
+            dt.mbtn = false;
+        }, //////////////// updateMore ///////////////////
     },
 });
 
