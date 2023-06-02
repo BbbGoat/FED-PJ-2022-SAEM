d3ee8f5ee57d9014b0e7ae09bb8207c83cfb9074
BbbGoat <oomi9421@gmail.com>
2023-06-02 16:16:46 +0900

    ss

---
+++ 014.React학습/react-app/wsm-app/src/dc/Games.js
@@ -0,0 +1,15 @@
+// DC 게임 페이지 컴포넌트
+import React from "react";
+import Ban from "./Ban";
+
+const Games = () => {
+
+    return(
+        <>
+            <Ban cat="GAMES" />
+        </>
+    );
+
+}; ////////////// Games /////////////////
+
+export default Games;
---
+++ 014.React학습/react-app/wsm-app/src/dc/Video.js
@@ -0,0 +1,16 @@
+// DC 비디오 페이지 컴포넌트
+import React from "react";
+import isrc from "./Imgsrc";
+
+const Video = () => {
+
+    return(
+        <>
+            <h2>Video 페이지</h2>
+            <iframe src={isrc.video} />
+        </>
+    );
+
+}; ////////////// Video /////////////////
+
+export default Video;
