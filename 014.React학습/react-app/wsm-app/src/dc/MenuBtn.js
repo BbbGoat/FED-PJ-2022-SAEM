332b63295cdf54ca812cf4ca769d0a66a67255be
BbbGoat <oomi9421@gmail.com>
2023-06-05 13:10:13 +0900

    ss

--- 014.React학습/react-app/wsm-app/src/dc/MenuBtn.js
+++ 014.React학습/react-app/wsm-app/src/dc/MenuBtn.js
@@ -13,15 +13,17 @@ function MenuBtn() {
     return (
         <>
             <section className="menubtn">
-                <div className="imbx">
-                    <img src="" alt="" />
-                </div>
-                <div className="titbx">
-                    <h3>//</h3>
-                    <h2>//</h2>
-                </div>
-                <div className="btnbx">
-                    <button>//</button>
+                <div>
+                    <div className="imbx">
+                        <img src="./images/menubtn-1.jpg" alt="메뉴버튼" />
+                    </div>
+                    <div className="titbx">
+                        <h3></h3>
+                        <h2></h2>
+                    </div>
+                    <div className="btnbx">
+                        <button></button>
+                    </div>
                 </div>
             </section>
             {/* 빈 루트를 만들고 JS로드함수를 포함
