58a0d9234da08f0483a779c1b0f322d7944852ab
BbbGoat <oomi9421@gmail.com>
2023-06-05 17:13:38 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/MenuBtn.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/MenuBtn.js
@@ -1,33 +1,44 @@
-// 메뉴 버튼 모듈 : MenuBtn.js
+///  메뉴버튼 모듈 - MenuBtn.js
 import $ from "jquery";
 import "./css/menubtn.css";
+import menubtn_data from "./data/menubtn";
+import {Link} from "react-router-dom";
 
-// 제이쿼리 로드구역 함수 ////////////
+// 제이쿼리 로드구역 함수 /////////
 function jqFn() {
-    $(()=>{
+    $(() => {
 
-    }); ///////////// jQB ///////////////////
-} /////////////// jqFn 함수 ///////////////////////
+
+    }); //////// jQB ///////////
+} ////////////// jQFn ///////////
 
 function MenuBtn() {
     return (
         <>
             <section className="menubtn">
-                <div>
-                    <div className="imbx">
-                        <img src="./images/menubtn-1.jpg" alt="메뉴버튼" />
+            {
+                menubtn_data.map((v,i)=>
+                    <div key={i}>
+                        <div className="imbx">
+                            <img src={v.isrc} alt="메뉴버튼" />
+                        </div>
+                        <div className="titbx">
+                            <h3>{v.tit.split('^')[0]}</h3>
+                            <h2>{v.tit.split('^')[1].toUpperCase()}</h2>
+                        </div>
+                        <div className="btnbx">
+                            {/* 라우터를 이용한 이동은 반드시 Link를 사용하자! */}
+                            <Link to={v.link}>
+                                <button>
+                                    {v.btn.toUpperCase()}
+                                </button>
+                            </Link>
+                        </div>
                     </div>
-                    <div className="titbx">
-                        <h3></h3>
-                        <h2></h2>
-                    </div>
-                    <div className="btnbx">
-                        <button></button>
-                    </div>
-                </div>
+                )
+                }
             </section>
-            {/* 빈 루트를 만들고 JS로드함수를 포함
-            -> 깔끔하게 section 밖으로 정리해주기 위함! */}
+            {/* 빈루트를 만들고 JS로드함수포함 */}
             {jqFn()}
         </>
     );
