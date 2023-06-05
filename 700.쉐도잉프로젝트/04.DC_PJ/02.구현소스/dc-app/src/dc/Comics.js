96bfd991be61cff5bb4a66ce31a9f4ec34d0cd4d
BbbGoat <oomi9421@gmail.com>
2023-06-05 11:46:00 +0900

    ss

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Comics.js
@@ -0,0 +1,15 @@
+// DC 코믹스 페이지 컴포넌트
+import React from "react";
+import Ban from "./Ban";
+
+const Comics = () => {
+
+    return(
+        <>
+            <Ban cat="COMICS" />
+        </>
+    );
+
+}; ////////////// Comics /////////////////
+
+export default Comics;
