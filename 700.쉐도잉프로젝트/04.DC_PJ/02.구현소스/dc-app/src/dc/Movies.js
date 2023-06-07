98f6eabced1b84b0e3d7a64289088fd83a0cc12e
BbbGoat <oomi9421@gmail.com>
2023-06-07 15:23:05 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Movies.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Movies.js
@@ -1,15 +1,15 @@
-// DC 무비 페이지 컴포넌트
+// DC MOVIES 페이지 컴포넌트
 import React from "react";
 import Ban from "./modules/Ban";
+import VidIntro from "./modules/VidIntro";
 
 const Movies = () => {
-
     return(
         <>
             <Ban cat="MOVIES" />
+            <VidIntro pg="MOVIES" mm="on" />
         </>
     );
-
-}; ////////////// Movies /////////////////
+}; /////////// Movies //////////////
 
 export default Movies;
--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Video.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Video.js
@@ -1,16 +1,13 @@
-// DC 비디오 페이지 컴포넌트
+// DC Video 페이지 컴포넌트
 import React from "react";
-import isrc from "./Imgsrc";
+import VidIntro from "./modules/VidIntro";
 
 const Video = () => {
-
     return(
         <>
-            <h2>Video 페이지</h2>
-            <iframe src={isrc.video} />
+            <VidIntro pg="VIDEO" mm="on" />
         </>
     );
-
-}; ////////////// Video /////////////////
+}; /////////// Video //////////////
 
 export default Video;
