e32ca342b811cb1c58f34f7d728a0398d701bef9
BbbGoat <oomi9421@gmail.com>
2023-06-07 12:34:36 +0900

    ss

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/vidintro.js
@@ -0,0 +1,62 @@
+// VidIntro 컴포넌트용 데이터 - vidintro.js
+
+const vidintro_data = {
+    main: {
+        vsrc: "https://www.youtube.com/embed/jprhe-cWKGs",
+        stit: "FEATURED VIDEO",
+        btit: "THE FLASH - FINAL TRAILER",
+        sum: "Are YOU ready? THE FLASH - Only in Theaters June 16.",
+        desc: "Advance tickets available now! * Flash hub for tickets and showtimes!",
+        link: ["Visit our official","https://www.dc.com/theflash"],
+    },
+    CHARACTERS: {
+        vsrc: "유튜브동영상경로",
+        stit: "작은타이틀",
+        btit: "큰타이틀",
+        sum: "요약소개",
+        desc: "설명",
+        link: "관련링크",
+    },
+    COMICS: {
+        vsrc: "유튜브동영상경로",
+        stit: "작은타이틀",
+        btit: "큰타이틀",
+        sum: "요약소개",
+        desc: "설명",
+        link: "관련링크",
+    },
+    "MOVIES & TV": {
+        vsrc: "유튜브동영상경로",
+        stit: "작은타이틀",
+        btit: "큰타이틀",
+        sum: "요약소개",
+        desc: "설명",
+        link: "관련링크",
+    },
+    GAMES: {
+        vsrc: "유튜브동영상경로",
+        stit: "작은타이틀",
+        btit: "큰타이틀",
+        sum: "요약소개",
+        desc: "설명",
+        link: "관련링크",
+    },
+    NEWS: {
+        vsrc: "유튜브동영상경로",
+        stit: "작은타이틀",
+        btit: "큰타이틀",
+        sum: "요약소개",
+        desc: "설명",
+        link: "관련링크",
+    },
+    VIDEO: {
+        vsrc: "유튜브동영상경로",
+        stit: "작은타이틀",
+        btit: "큰타이틀",
+        sum: "요약소개",
+        desc: "설명",
+        link: "관련링크",
+    },
+};
+
+export default vidintro_data;
---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/VidIntro.js
@@ -0,0 +1,51 @@
+// VidIntro 컴포넌트 JS - VidIntro.js
+
+import $ from "jquery";
+import "../css/vidintro.css";
+import vidintro_data from "../data/vidintro";
+
+// 제이쿼리 로드구역 함수 /////////
+function jqFn() {
+    $(() => {
+
+
+    }); //////// jQB ///////////
+} ////////////// jQFn ///////////
+
+function VidIntro(props) {
+    // props.pg - 해당페이지 데이터속성명
+
+    // 데이터 선택하기
+    const sdt = vidintro_data[props.pg];
+    
+    return (
+        <>
+            {/* 모듈코드 */}
+            <section className="vidbox">
+                {/* 비디오파트 */}
+                <div className="vb1">
+                <iframe src={sdt.vsrc} title={sdt.stit}></iframe>
+                </div>
+                {/* 타이틀파트 */}
+                <div className="vb2">
+                    <h3>{sdt.stit}</h3>
+                    <h2>{sdt.btit}</h2>
+                    <p>{sdt.sum}</p>
+                    <p className="desc">
+                        {sdt.desc.split("*")[0]}
+                        <a href={sdt.link[1]} target="_blank">
+                            {sdt.link[0]}
+                        </a>
+                        {sdt.desc.split("*")[1]}
+                    </p>
+                    {/* 링크있을경우 표시 */}
+                </div>
+            </section>
+            {/* 빈 루트를 만들고 JS로드함수를 포함
+            -> 깔끔하게 section 밖으로 정리해주기 위함! */}
+            {jqFn()}
+        </>
+    );
+}
+
+export default VidIntro;
